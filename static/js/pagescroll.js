e261977e1af21ec3de0f7f34b31a6beb53135ee4
David Zilbrug <davidzilburg@Davids-MacBook-Pro.local>
2016-08-15 10:34:42 -0400

    info updates and js updates

--- static/js/pagescroll.js
+++ static/js/pagescroll.js
@@ -25,6 +25,24 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+//If navbar-toggle is not visible on initial load, make sure it becomes visible when necessary
+$(window).resize(function(){
+    if($(window).width() <= 767){
+        $('.navbar-toggle:visible').unbind("click");
+        //Make navbar dark if menu opened at top
+        $('.navbar-toggle:visible').click(function() {
+            console.log("navbar clicked");
+            setTimeout(function(){
+                if($(".navbar-collapse").attr("aria-expanded") == "true"){
+                    $(".navbar-default").addClass("navbar-dark");
+                }else{
+                    $(".navbar-default").removeClass("navbar-dark");
+                }
+            }, 75);
+        });
+    }
+});
+//Make navbar dark if menu opened at top
 $('.navbar-toggle:visible').click(function() {
 	console.log("navbar clicked");
     setTimeout(function(){
